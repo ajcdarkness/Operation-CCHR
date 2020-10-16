8441036506d7bb2ae080fb8194d1f271e02e4c4a
ajcdarkness <33042703+ajcdarkness@users.noreply.github.com>
2020-10-15 22:30:27 -0400

    Update newUser.sh

--- newUser.sh
+++ newUser.sh
@@ -2,3 +2,23 @@ QUIET (){
     eval $@ 2>/dev/null >/dev/null
     return $?
 }
+users_add() {
+    # Add all the following users add wheel/sudo with a password changem
+    for user in "kong" "thanos" "deadpool" "bane" "vader" "yondu"; do
+        QUIET useradd $user;
+        s1=$?;
+        echo "$user:changeme" | chpasswd 2>/dev/null >/dev/null;
+        s2=$?;
+        QUIET usermod -G `grep -oE "wheel|sudo" /etc/group` $user;
+        s3=$?;
+        if [ "$s3$s2$s1" != "000" ]; then
+            fail="$fail $user";
+        fi;
+    done;
+    if [ "$fail" != "" ]; then
+        echo "failed to add users $fail";
+    else
+        echo "Added users"
+    fi;
+
+}
